55189d8225ba02a4b948161d4b3c1d786effbaaa
Eduardo Rahn <eduardo1590@gmail.com>
2021-04-01 16:11:51 -0400

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(DogSeeder::class);
-        //$this->call(WeightSeeder::class);
+        //$this->call(DogSeeder::class);
+        $this->call(WeightSeeder::class);
     }
 }
